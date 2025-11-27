003faf1fc2c515c9279b0d0c56b75de77a3df9eb
jonas <joni0014@stud.ek.dk>
2025-11-27 23:41:49 +0100

    Multiply

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-
-
+//Selectors
 const firstInput = document.querySelector("#inputOne")
 const secondInput = document.querySelector("#inputTwo")
 const buttonOnPage = document.querySelector("#button")
 
-
+//Plus
 function addTwoValues(firstInput, secondInput) {
     return (Math.round(firstInput.value) + Math.round(secondInput.value))
 }
@@ -20,6 +19,34 @@ buttonOnPage.addEventListener("click", () => {
     renderOnPage.innerText = result
 
 
-//Effekter til calc
+//Effekter til calc i plus
 renderOnPage.style.border = "5px solid green";
+});
+
+//Gange
+const renderMultiply = document.querySelector("#resultMultiply")
+const buttonMultiply = document.querySelector("#buttonMultiply")
+const inputOne =document.querySelector("#inputOneMultiply")
+const inputTwo =document.querySelector("#inputTwoMultiply")
+
+function multiply(inputOne, inputTwo) {
+
+    if (Math.round(inputOne.value) === 0) {
+        return window.alert("Resultatet giver altid 0, hvis du ganger med 0. Dumb, Dumb!")
+    } else if (Math.round(inputTwo.value) === 0) {
+        return window.alert("Resultatet giver altid 0, hvis du ganger med 0. Dumb, Dumb!") }
+    else {
+        return (Math.round(inputOne.value) * Math.round(inputTwo.value))
+    }}
+
+
+
+buttonMultiply.addEventListener("click", function() {
+    console.log("Gange funktion anvendt!")
+    let resultMultiply = multiply(inputOne, inputTwo)
+    console.log(multiply(inputOne, inputTwo))
+    renderMultiply.innerText=resultMultiply
+
+//Effekt til Multyiply
+renderMultiply.style.border= "5px solid green"
 });
