1a6aca032cfd968474268dcdb88ad117b90c59ae
jonas <joni0014@stud.ek.dk>
2025-11-29 23:15:41 +0100

    media

--- main.js
+++ main.js
@@ -20,33 +20,60 @@ buttonOnPage.addEventListener("click", () => {
 
 
 //Effekter til calc i plus
-renderOnPage.style.border = "5px solid green";
+    renderOnPage.style.border = "5px solid green";
 });
 
 //Gange
 const renderMultiply = document.querySelector("#resultMultiply")
 const buttonMultiply = document.querySelector("#buttonMultiply")
-const inputOne =document.querySelector("#inputOneMultiply")
-const inputTwo =document.querySelector("#inputTwoMultiply")
+const inputOne = document.querySelector("#inputOneMultiply")
+const inputTwo = document.querySelector("#inputTwoMultiply")
 
 function multiply(inputOne, inputTwo) {
 
     if (Math.round(inputOne.value) === 0) {
         return window.alert("Resultatet giver altid 0, hvis du ganger med 0. Dumb, Dumb!")
     } else if (Math.round(inputTwo.value) === 0) {
-        return window.alert("Resultatet giver altid 0, hvis du ganger med 0. Dumb, Dumb!") }
-    else {
+        return window.alert("Resultatet giver altid 0, hvis du ganger med 0. Dumb, Dumb!")
+    } else {
         return (Math.round(inputOne.value) * Math.round(inputTwo.value))
-    }}
+    }
+}
 
 
-
-buttonMultiply.addEventListener("click", function() {
+buttonMultiply.addEventListener("click", function () {
     console.log("Gange funktion anvendt!")
     let resultMultiply = multiply(inputOne, inputTwo)
     console.log(multiply(inputOne, inputTwo))
-    renderMultiply.innerText=resultMultiply
+    renderMultiply.innerText = resultMultiply
 
 //Effekt til Multyiply
-renderMultiply.style.border= "5px solid green"
+    renderMultiply.style.border = "5px solid green"
 });
+//Division
+const inputOneDivision = document.querySelector("#inputOneDivision");
+const inputTwoDivision = document.querySelector("#inputTwoDivision")
+const divisionToButton = document.querySelector("#buttonDivision")
+
+divisionToButton.addEventListener("click", function () {
+console.log("Division activated")
+    function division(inputOneDivision, inputTwoDivision) {
+        if (Math.round(inputOneDivision.value) === 0) {
+            return window.alert("Du kan ikke benytte 0 i Division. Lærte du ikke fra Gange delen? 6 7 dumb, dumb!")
+        } else if (Math.round(inputTwoDivision.value) === 0) {
+            return window.alert("Du kan ikke benytte 0 i Division. Lærte du ikke fra Gange delen? 6 7 dumb, dumb!")
+        } else {
+            return (Math.round(inputOneDivision.value) / Math.round(inputTwoDivision.value))
+        }
+    }
+    console.log(inputOneDivision.value)
+    console.log(inputTwoDivision.value)
+
+    renderDivision = document.querySelector("#resultDivision")
+    let divisionResult = division(inputOneDivision, inputTwoDivision)
+    renderDivision.innerText = divisionResult
+    renderDivision.style.border = "solid green 5px"
+});
+
+
+
